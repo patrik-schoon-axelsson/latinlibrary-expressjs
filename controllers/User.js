4ba2ddf40a22986f1f54efabcd35f39127882cba
Patrik Schöön-Axelsson <paxicon84@gmail.com>
2021-11-11 19:44:59 +0100

    Added error handling for User controller

--- controllers/User.js
+++ controllers/User.js
@@ -6,22 +6,24 @@ const { response } = require('express');
 
 exports.register = async (req, res) => {
     try {
-    let hashpass = await bcrypt.hash(req.body.password, 15)
-    let new_user = User.build({
-        email: req.body.email,
-        userName: req.body.userName,
-        password: hashpass,
-        admin: false
-    })
-
-    new_user.save()
-    .then(user => {
-        res.status(200).json(user)
-    }).catch(err => {
-        res.status(500).json({
-            message: err.message
+        let hashpass = await bcrypt.hash(req.body.password, 15)
+        let new_user = User.build({
+            email: req.body.email,
+            userName: req.body.userName,
+            password: hashpass,
+            admin: false
         })
-    })
+
+        new_user.save()
+        .then(user => {
+            res.status(200).json({
+                message: "New user added succesfully."
+            })
+        }).catch(err => {
+            res.status(500).json({
+                message: err.message
+            })
+        })
     } catch (err) {
         res.status(500).json({
             message: err.message
@@ -36,17 +38,17 @@ exports.login = async (req, res) => {
         const password_valid = bcrypt.compareSync(req.body.password, user.password)
         
         if (password_valid) {
-            const sanitized_user = {
-                email: user.email,
-                userName: user.userName,
-                admin: user.admin
-            }
-            const access_token = await jwt.sign(sanitized_user, process.env.JWT_SECRET)
+                const sanitized_user = {
+                    email: user.email,
+                    userName: user.userName,
+                    admin: user.admin
+                }
+                const access_token = await jwt.sign(sanitized_user, process.env.JWT_SECRET)
 
-            res.status(200).json({
-                token: access_token,
-                user: sanitized_user
-            })
+                res.status(200).json({
+                    token: access_token,
+                    user: sanitized_user
+                })
             } else {
                 res.status(403).json({
                     message: "Invalid credentials."
